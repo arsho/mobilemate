8bbccaf7dfefdfd69c5c6f136f1a6ff49cbdd43c
Ahmedur Rahman Shovon <shovon.sylhet@gmail.com>
2017-11-05 15:31:49 +0600

    testing local files

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask
-from config import DevelopmentConfig
+from config import *
 
 app = Flask(__name__)
-app.config.from_object(DevelopmentConfig)
+
+greetings = "Production greetings"
+
+try:
+    from instance import windows_development_configuration
+    app.config.from_object(DevelopmentConfig)
+    greetings = windows_development_configuration.greetings
+except:
+    app.config.from_object(ProductionConfig)
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "Hello mobilemate"
+    return "Hello mobilemate, from "+greetings
 
 if __name__ == '__main__':
     app.run()
